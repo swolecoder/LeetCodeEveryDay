b41df438aa05c421c4565ffb3aabc5c17f9f1eef
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-01-02 18:33:11 -0800

    Time: 31 ms (64.82%), Space: 73.6 MB (31.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::set<int> seen;
-        for(int num:nums){
+        std::unordered_set<int> seen;
+
+        for(auto num: nums){
             if(seen.find(num) != seen.end()){
                 return true;
             }
             seen.insert(num);
         }
+
         return false;
         
     }
