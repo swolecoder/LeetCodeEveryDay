6e1e5054e2a6f012c5cb5c0683f034ddff9be78a
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-01-03 12:01:29 -0800

    Time: 3 ms (60.21%), Space: 9.8 MB (30.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,9 +6,30 @@ public:
             return false;
         }
 
-        std::sort(s.begin(), s.end());
-        std::sort(t.begin(), t.end());
-        return s == t;
+        // std::sort(s.begin(), s.end());
+        // std::sort(t.begin(), t.end());
+        // return s == t;
+
+        std::unordered_map<char, int> dict;
+
+        for(auto& ch:s){
+            dict[ch]++;
+        }
+
+
+        for(auto& ch: t){
+            if(dict.find(ch) == dict.end()){
+                return false;
+            }
+            dict[ch] -=1;
+            if(dict[ch] == 0){
+                 dict.erase(ch);
+
+            }
+
+        }
+
+        return dict.empty();
         
     }
 };
