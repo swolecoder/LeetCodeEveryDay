cf292a98ab57cd64b6b703fb114b63a535f8da91
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-01-10 15:11:49 -0800

    Time: 0 ms (100%), Space: 23.1 MB (21.79%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,11 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-
-        l = 0
-        r = len(s)-1
-
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l +=1
-            r -=1
+        i=0
+        j = len(s)-1
+        while i < j:
+            s[i],s[j] = s[j],s[i]
+            i +=1
+            j -=1
+        
