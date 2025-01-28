51cd142b98e954b33d6c199978139c26042a2b07
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-01-27 20:27:58 -0800

    Time: 0 ms (100%), Space: 20.7 MB (59.49%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int size = nums.size();
+        int x = 0;
+        for(int i =0; i < size; i++){
+            x ^= nums[i];
+
+        }
+        return x;
+
+    }
+};
