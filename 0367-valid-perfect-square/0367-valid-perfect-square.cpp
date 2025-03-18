138c4c3fb1fe6cf144559022301a5443d8b8b4b1
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-03-18 11:43:02 -0700

    Time: 0 ms (100%), Space: 7.6 MB (99.38%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -2,7 +2,16 @@ class Solution {
 public:
     bool isPerfectSquare(int num) {
         int a = (int) sqrt(num);
-        return a * a == num;
+        for(long long i=1; i <= a; i++){
+            long long d = i * i;
+            if(d > num){
+                return false;
+            }
+            if(d == num){
+                return true;
+            }
+        }
+        return false;
         
     }
 };
