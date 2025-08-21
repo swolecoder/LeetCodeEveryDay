a356ee5cafca40130d283ab480ecbdd35540cc47
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-08-20 22:50:27 -0700

    Time: 5 ms (47.96%) | Memory: 17.7 MB (92.44%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        r = defaultdict(set)
+        c = defaultdict(set) 
+        rc = defaultdict(set)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                val = board[i][j]
+                if board[i][j] == ".":
+                    continue
+
+
+                if val in r[i] or val in c[j] or val in rc[(i//3,j//3)]:
+                    return False
+                r[i].add(val)
+                c[j].add(val)
+                rc[(i//3,j//3)].add(val)     
+        return True  
