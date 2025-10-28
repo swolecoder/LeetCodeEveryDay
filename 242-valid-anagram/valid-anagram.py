b24a9c67280775732935523be2ec8d062500b512
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-10-27 21:14:03 -0700

    Time: 10 ms (74.61%) | Memory: 17.6 MB (97.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,8 +3,26 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        A = sorted(s)
-        B = sorted(t)
+        # A = sorted(s)
+        # B = sorted(t)
 
-        return A == B
+        # return A == B
+
+        mapA = {}
+        mapB = {}
+
+        for ch in s:
+            mapA[ch] = mapA.get(ch, 0)+1
+        
+        for ch in t:
+            mapB[ch] = mapB.get(ch,0) +1
+
+
+        
+
+        
+        
+        print(mapA)
+
+        return mapA == mapB
         
