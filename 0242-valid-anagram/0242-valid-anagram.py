a77cd59fcee0c08809696d231fd1f7420fcb2145
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2025-10-27 21:14:01 -0700

    Time: 10 ms (74.61%), Space: 17.6 MB (97.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict = defaultdict(int)
         if len(s) != len(t):
             return False
+        
+        # A = sorted(s)
+        # B = sorted(t)
+
+        # return A == B
+
+        mapA = {}
+        mapB = {}
 
         for ch in s:
-            dict[ch] +=1
+            mapA[ch] = mapA.get(ch, 0)+1
         
-        print(dict)
-
         for ch in t:
-            if ch not in dict:
-                return False
-            dict[ch] -=1
-            if dict[ch] == 0:
-                del dict[ch]
-        return True
+            mapB[ch] = mapB.get(ch,0) +1
 
-        # return ''.join(sorted(s)) == ''.join(sorted(t))
+
+        
+
+        
+        
+        print(mapA)
+
+        return mapA == mapB
         
