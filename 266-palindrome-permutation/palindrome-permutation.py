95975c8251b7660b4a6d09e1d2895ed7b7208c7f
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2026-02-25 23:23:08 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (57.92%) - LeetSync

--- 266-palindrome-permutation/palindrome-permutation.py
+++ 266-palindrome-permutation/palindrome-permutation.py
@@ -1,14 +1,24 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
-        map = Counter(s)
+
+        seen = set()
 
 
-        odd = 0
+        for ch in s:
+            if ch in seen:
+                seen.remove(ch)
+            else:
+                seen.add(ch)
+        return len(seen) <= 1
+        # map = Counter(s)
 
-        for ch, count in map.items():
-            if count % 2 == 1:
-                odd +=1
 
-                if odd > 1:
-                    return False
-        return True
+        # odd = 0
+
+        # for ch, count in map.items():
+        #     if count % 2 == 1:
+        #         odd +=1
+
+        #         if odd > 1:
+        #             return False
+        # return True
